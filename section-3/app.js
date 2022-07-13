72148183fe92dcbf8eae1a08fb762fd3561cfaa4
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-13 17:25:59 +0530

    section 3 | lecture 4/18 | nodejs code lifecycle and ending event loop

--- section-3/app.js
+++ section-3/app.js
@@ -6,6 +6,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('req: ', req);
+    process.exit(); // to exit from the current process and stop the listeners hence ending event loop
 })
 
 server.listen(3000);

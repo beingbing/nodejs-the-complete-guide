77409c5c492be008cb28369bdc226cc639022b1f
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-13 17:35:31 +0530

    section 3 | lecture 7/18 | sending responses

--- section-3/app.js
+++ section-3/app.js
@@ -6,7 +6,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('req: ', req.url, req.method, req.headers);
-    process.exit(); // to exit from the current process and stop the listeners hence ending event loop
+    // process.exit(); // to exit from the current process and stop the listeners hence ending event loop
+    res.setHeader('Content-Type', 'text/html');
+    res.write('{name: samar, age: 27, activity: study}');
+    res.end();
 })
 
 server.listen(3000);

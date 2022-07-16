c0147a7aa119909bbbe014c70c8d10452c49360b
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-16 15:25:57 +0530

    section 5 | lecture 11/23 | routing in express application

---
+++ section-05/05-using-express-router/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen(3000);
+
+// get() and post() do an exact match, whereas use() matches prefix of a path

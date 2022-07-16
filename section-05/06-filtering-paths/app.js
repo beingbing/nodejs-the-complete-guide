cadaa95aad2d0daee04ebea6663ce91317cb0076
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-16 15:36:29 +0530

    section 5 | lecture 13/23 | filtering out common prefixes from url

---
+++ section-05/06-filtering-paths/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// adding a common segment route as a filter so, all /admin/... will now go in this file
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000);

f9454fb1b08446a4764295e15440cbd25df9ce8e
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-16 15:30:04 +0530

    section 5 | lecture 12/23 | handling 404 request

--- section-05/05-using-express-router/app.js
+++ section-05/05-using-express-router/app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<p> page not found </p>')
+})
+
 app.listen(3000);
 
 // get() and post() do an exact match, whereas use() matches prefix of a path

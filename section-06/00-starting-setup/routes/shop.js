504cbdef5bae93921e3ebdc7f0ceac57404e2f3f
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-16 21:49:51 +0530

    section 6 | lecture 2/19 | keeping data in variables and sharing it across users

---
+++ section-06/00-starting-setup/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const { products } = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  console.log("admin data: ", products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;

cd57dcbcfcf0f985a627279ea9ecc1b57c5c1d14
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-17 10:46:36 +0530

    section 6 | lecture 9/19 | pug templates rendered

--- section-06/00-starting-setup/routes/shop.js
+++ section-06/00-starting-setup/routes/shop.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   console.log("admin data: ", products);
-  res.render("shop");
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;

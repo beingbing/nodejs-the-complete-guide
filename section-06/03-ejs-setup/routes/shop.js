0b8521da08ba729639c27ca3bae1142359757794
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-17 13:54:05 +0530

    section 6 | lecture 16/19 | ejs templating engine

--- section-06/03-ejs-setup/routes/shop.js
+++ section-06/03-ejs-setup/routes/shop.js
@@ -3,11 +3,16 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const {products} = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;

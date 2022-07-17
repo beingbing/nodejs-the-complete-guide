e2f2e71beba72dd86eb5f6a4675c17da2bf4a5cb
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-17 16:29:18 +0530

    section 7 | lecture 8/10 | fetching data from file via model

--- section-07/models/product.js
+++ section-07/models/product.js
@@ -1,24 +1,46 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        // 'this' will refer to the object created based on
-        // the class. And that is the object we want to store
-        // in this array. 
-        products.push(this);
-    }
+  save() {
+    getProductsFromFile((products) => {
+      // 'this' will refer to the object created based on
+      // the class. And that is the object we want to store
+      // in this array.
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    // it's a utility function, should not be called for
-    // a single instance of the Product class, because it
-    // should fetch all products. And i don't want to create
-    // a new object with the 'new' keyword with some dummy
-    // title just to fetch all existing products. Hence,
-    // i will keep it static
-    static fetchAll() {
-        return products;
-    }
-}
+  // it's a utility function, should not be called for
+  // a single instance of the Product class, because it
+  // should fetch all products. And i don't want to create
+  // a new object with the 'new' keyword with some dummy
+  // title just to fetch all existing products. Hence,
+  // i will keep it static
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};

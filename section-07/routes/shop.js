551211ba921709a94d525029acbd1da9480b3a7d
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-07-17 15:45:34 +0530

    section 7 | lecture 3/10 | adding controller

--- section-07/routes/shop.js
+++ section-07/routes/shop.js
@@ -2,18 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const prodController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-  });
-});
+router.get('/', prodController.getProducts);
 
 module.exports = router;
